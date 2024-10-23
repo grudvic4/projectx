bade493b8093c04d5e212804e54cca18a118163b
grudvic4 <salihbegk@gmail.com>
2024-10-23 20:17:24 +0200

    Fix page

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/projectx/' : '/' // Replace 'projectx' with your repository name
 });
